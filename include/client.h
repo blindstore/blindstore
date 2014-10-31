8bf56255779bd79385596503b80d9a1390edf3f0
OmeGak <omegak@gmail.com>
2014-10-31 17:23:15 +0100

    Initial commit

---
+++ include/client.h
@@ -0,0 +1,7 @@
+#ifndef __CLIENT_H__
+#define __CLIENT_H__
+
+char* generate_query(const unsigned int index, const unsigned int index_size);
+char* resolve_answer(const char* encrypted_answer);
+
+#endif
