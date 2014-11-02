457176862d1cde99dcbeeb0c65fc15722f880d7f
OmeGak <omegak@gmail.com>
2014-11-02 18:37:32 +0100

    Move client.h into include/client

--- include/client/client.h
+++ include/client/client.h
@@ -1,7 +1,7 @@
 #ifndef __CLIENT_H__
 #define __CLIENT_H__
 
-char* generate_query(const unsigned int index, const unsigned int index_size);
+char* generate_query(const char* key, const unsigned int capacity, const char* she_secret_key);
 char* resolve_answer(const char* encrypted_answer);
 
 #endif
